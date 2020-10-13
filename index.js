d9b6d6d0715b6721ff461aa09d28d58d0d4a9e0f
majakulpa <majaweronikakulpa@gmail.com>
2020-10-14 00:58:38 +1100

    changes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const app = require('./app')
 const http = require('http')
-const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-const myPORT = process.env.PORT || config.PORT
-server.listen(myPORT, () => {
-  logger.info(`Server is running on port ${myPORT}`)
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`)
 })
